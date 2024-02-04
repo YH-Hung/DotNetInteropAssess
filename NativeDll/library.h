a8d6352475786d838abf915302e8811629c9ed7b
yinghanhung <hle.infdy@gmail.com>
2024-02-05 00:43:54 +0800

    init

---
+++ NativeDll/library.h
@@ -0,0 +1,6 @@
+#ifndef NATIVEDLL_LIBRARY_H
+#define NATIVEDLL_LIBRARY_H
+
+extern "C" void hello();
+
+#endif //NATIVEDLL_LIBRARY_H
---
+++ NativeInteropAssess/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+[DllImport("/Users/yinghanhung/Projects/dotnet/NativeInteropAssess/NativeDll/cmake-build-debug/libNativeDll.dylib")]
+static extern void hello();
+
+hello();
+Console.WriteLine("Hello, World!");
